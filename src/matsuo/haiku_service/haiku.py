28e9264ca5d646c51bbdbacc5cf3644b62fd4ff1
Nicolas Primeau <icolas.primeau@gmail.com>
2018-02-17 18:29:15 -0500

    k

--- src/matsuo/haiku_service/haiku.py
+++ src/matsuo/haiku_service/haiku.py
@@ -1,3 +1,4 @@
+from textstat import textstat
 
 
 def compute_haiku_coherence(haiku):
@@ -7,13 +8,22 @@ def compute_haiku_coherence(haiku):
 class Haiku:
 
     def __init__(self, lines):
-        if len(lines) != 3:
-            raise ValueError('Not a haiku')
         self.lines = lines
-        self._coherence_score = None
+        self.validate()
+
+    def validate(self):
+        if len(self.lines) != 3:
+            raise ValueError('Not a haiku')
+        constraints = [5, 7, 5]
+        for idx in range(len(self.lines)):
+            syllables = 0
+            for word in self.lines[idx]:
+                syllables += textstat.textstat.syllable_count(word)
+            if syllables > constraints[idx]:
+                raise ValueError('Not a haiku')
 
     def __repr__(self):
         return str(self)
 
     def __str__(self):
-        return'\n'.join(self.lines)
+        return ',\n'.join(map(lambda line: ' '.join(map(lambda word: word.capitalize(), line)), self.lines))
--- src/matsuo/haiku_service/solver.py
+++ src/matsuo/haiku_service/solver.py
@@ -7,43 +7,91 @@ from deap import tools
 from nltk.corpus import brown
 from textstat import textstat
 
-words = set(brown.words())
+from matsuo.haiku_service.haiku import Haiku
+
+words = list(set(brown.words()))
 keywords = None
 
 
+class Fitness:
+    def __init__(self):
+        self.values = list()
+        self.valid = False
+
+    def __lt__(self, other):
+        return self.values < other.values
+
+    def __le__(self, other):
+        return self.values <= other.values
+
+    def __eq__(self, other):
+        return self.values == other.values
+
+    def __ne__(self, other):
+        return self.values != other.values
+
+    def __gt__(self, other):
+        return self.values > other.values
+
+    def __ge__(self, other):
+        return self.values >= other.values
+
+
+class Individual:
+
+    def __init__(self, lines):
+        self.lines = lines
+        self.fitness = Fitness()
+
+    def __copy__(self):
+        return Individual([list(line) for line in self.lines])
+
+
 def _mutate(individual):
-    fxs = []
-    can_add
-    fx_idx = random.randint(0, len(fxs) - 1)
+    fxs = [add_word, delete_word, jumble_words]
+    random.choice(fxs)(individual)
 
 
+def add_word(individual):
+    random.choice(individual.lines).insert(random.randint(0, len(individual.lines)-1), random.choice(words))
+
+
+def delete_word(individual):
+    try:
+        random.choice(individual.lines).remove(random.randint(0, len(individual.lines) - 1))
+    except Exception:
+        pass
+
+
+def jumble_words(individual):
+    random.shuffle(random.choice(individual.lines))
 
 
 def _crossover(individual1, individual2):
     new_ind1 = [[], [], []]
     new_ind2 = [[], [], []]
-    for idx in len(individual1):
+    for idx in range(len(individual1.lines)):
         if random.randint(0, 1) == 0:
-            individual1[idx], individual2[idx] = individual1[idx], individual2[idx]
+            individual1.lines[idx], individual2.lines[idx] = individual1.lines[idx], individual2.lines[idx]
         else:
-            individual1[idx], individual2[idx] = individual2[idx], individual1[idx]
+            individual1.lines[idx], individual2.lines[idx] = individual2.lines[idx], individual1.lines[idx]
     return new_ind1, new_ind2
 
 
 def _coherence_score(individual):
-    if len(individual) != 3:
+    if len(individual.lines) != 3:
         return -1
     constraints = [5, 7, 5]
     line_lengths = [0, 0, 0]
-    for idx in len(line_lengths):
-        for word in individual[idx]:
+    for idx in range(len(line_lengths)):
+        for word in individual.lines[idx]:
             line_lengths[idx] += textstat.textstat.syllable_count(word)
 
     syllable_fitness = 0
-    for idx in line_lengths:
+    for idx in range(len(line_lengths)):
         syllable_fitness += 1 - (1 / len(line_lengths)) * abs(line_lengths[idx] - constraints[idx])
     words = list()
-    for line in individual:
+    for line in individual.lines:
         words.extend(line)
 
     words = set(words)
@@ -53,25 +101,25 @@ def _coherence_score(individual):
             occurance += 1
     keyword_fitness = 1 - 1/occurance
 
-    return keyword_fitness * syllable_fitness
+    return [keyword_fitness * syllable_fitness]
 
 
 def _create_individual():
-    return [[random.choice(keywords)], [random.choice(keywords)], [random.choice(keywords)]]
+    return Individual([[random.choice(keywords)], [random.choice(keywords)], [random.choice(keywords)]])
 
 
-def create_haiku_with_ga(kws, mutation_prob=0.05, crossover_prob=0.1):
+def create_haiku_with_ga(kws, mutation_prob=0.05, crossover_prob=0.1, max_gen=500):
     global keywords
     keywords = kws
     creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMax)
+    creator.create("Individual", Individual, fitness=creator.FitnessMax)
     toolbox = base.Toolbox()
     # Structure initializers
-    toolbox.register("individual", tools.initRepeat, creator.Individual, _create_individual)
+    toolbox.register("individual", _create_individual)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
     toolbox.register("evaluate", _coherence_score)
     toolbox.register("mate", _crossover)
-    toolbox.register("mutate", _mutate, indpb=0.05)
+    toolbox.register("mutate", _mutate)
     toolbox.register("select", tools.selTournament, tournsize=3)
 
     pop = toolbox.population(n=300)
@@ -84,14 +132,14 @@ def create_haiku_with_ga(kws, mutation_prob=0.05, crossover_prob=0.1):
     g = 0
 
     # Begin the evolution
-    while max(fits) < 100 and g < 1000:
+    while max(fits) < 100 and g < max_gen:
         # A new generation
         g = g + 1
         print("-- Generation %i --" % g)
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
         # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
+        offspring = list(map(lambda ind: ind.__copy__(), offspring))
         # Apply crossover and mutation on the offspring
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
             if random.random() < crossover_prob:
@@ -102,7 +150,10 @@ def create_haiku_with_ga(kws, mutation_prob=0.05, crossover_prob=0.1):
         for mutant in offspring:
             if random.random() < mutation_prob:
                 toolbox.mutate(mutant)
-                del mutant.fitness.values
+                try:
+                    del mutant.fitness.values
+                except AttributeError:
+                    pass
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
@@ -121,8 +172,22 @@ def create_haiku_with_ga(kws, mutation_prob=0.05, crossover_prob=0.1):
         print("  Max %s" % max(fits))
         print("  Avg %s" % mean)
         print("  Std %s" % std)
-    return pop[max(fits)]
+
+    sorted_idxs = sorted(range(len(fits)), key=lambda idx: fits[idx])
+    sorted_pop = [pop[idx] for idx in sorted_idxs]
+
+    while len(sorted_pop) > 0:
+        try:
+            return Haiku(sorted_pop.pop(-1).lines)
+        except ValueError:
+            pass
+    return None
 
 
-def create_haiku(keywords, fx=create_haiku_with_ga, **kwargs):
-    return fx(keywords, kwargs)
+def create_haiku(kes, fx=create_haiku_with_ga):
+    return fx(kes)
+
+
+if __name__ == "__main__":
+    keys = set(random.choice(words) for i in range(20))
+    print(create_haiku(['student', 'school', 'phone', 'pen', 'power']))
