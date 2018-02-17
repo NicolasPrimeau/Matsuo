445b31f8c5f9668c23ce80441efdb85b8b06d6cf
Nicolas Primeau <icolas.primeau@gmail.com>
2018-02-17 17:15:49 -0500

    k

--- src/matsuo/coordinator_service/app.py
+++ src/matsuo/coordinator_service/app.py
@@ -1,10 +1,14 @@
 import os
 from flask import Flask, render_template, request
+
+from matsuo.haiku_service.haiku_service import HaikuService
 from matsuo.utils import requests
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
     return render_template("upload.html")
@@ -15,6 +19,7 @@ def upload():
     img = request.files.getlist("file")
     requests.get(HaikuService.SERVICE_NAME, 'get_haiku', {img})
     return render_template("complete.html")
+
 
 if __name__== "__main__":
     app.run(port=3333, debug=True)
