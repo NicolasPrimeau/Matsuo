58cece3889336ae83f269046334625b6f6294718
kenmurrell <ken.murrell13@gmail.com>
2018-02-18 10:46:50 -0500

    ui prototype

--- src/matsuo/service_base/test2.py
+++ src/matsuo/service_base/test2.py
@@ -1,26 +1,53 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, send_from_directory
 from random import randint
 from werkzeug.utils import secure_filename
 import os
+from PIL import Image
 
-app = Flask(__name__, static_folder='images')
-APP_ROT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)),"uploads")
+app = Flask(__name__)
+app.static_folder = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+current_filename =''
 
 @app.route("/")
 def index():
     return render_template("upload.html")
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 @app.route("/upload", methods=['POST'])
 def upload():
     if request.method == 'POST':
+        print(request.form['submit'])
+        print(request.form.get('gen'))
         pic = request.files['file']
-        pic.save("./images/test.jpg")
+        if pic and allowed_file(pic.filename):
+            pic.save(os.path.join(UPLOAD_FOLDER, pic.filename))
+            img = Image.open(os.path.join(UPLOAD_FOLDER, pic.filename))
+            img.thumbnail((300,300), resample=0)
+            img.save(os.path.join(UPLOAD_FOLDER, pic.filename))
+            return redirect(url_for('uploaded_file', filename=pic.filename))
         return redirect(url_for('main'))
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    current_filename = filename
+    return render_template('template1.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+     return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/')
+def generate(filename):
+    print(filename)
+    return
 
 @app.route("/main",methods=['GET', 'POST'])
 def main():
-    pic_filename = "test.jpg"
-    text = "Please submit a picture!"
     return render_template('template1.html',**locals())
 
 if __name__ == "__main__":
