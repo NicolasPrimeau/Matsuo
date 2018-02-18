f77dd0ab909051b291ec26f86ce193d3bb7934ed
Nicolas Primeau <icolas.primeau@gmail.com>
2018-02-17 21:23:19 -0500

    k

--- src/matsuo/haiku_judge/main.py
+++ src/matsuo/haiku_judge/main.py
@@ -1,3 +1,16 @@
 
+import json
 
 
+from nltk.tag import StanfordPOSTagger
+jar = 'stanford-model/stanford-postagger-3.7.0.jar'
+model = 'stanford-models/models/french.tagger'
+
+pos_tagger = StanfordPOSTagger(model, jar, encoding='utf8')
+haikus = json.load(open('data/haikus.json'))
+
+for haiku in haikus:
+    for line in haiku:
+        res = pos_tagger.tag(line.split())
+        print(res)
+        input()
